ad42fbf117881aa133477fb77d2bdcd02bae3b83
SalvatoreAddeo <s.addeo5@studenti.unisa.it>
2026-06-02 12:39:40 +0200

    Feat: implementati comandi Add e Modify e centralizzato il receiver
    - Aggiunto il metodo updateTrack nella classe Library;
    - Create le classi ConcreteCommand AddTrackCommand e ModifyTrackCommand secondo le specifiche del pattern.
    - Aggiornato AddModTrackController per configurare i comandi e delegare l'esecuzione all'Invoker.

--- src/main/java/com/Command/AddTrack.java
+++ src/main/java/com/Command/AddTrack.java
@@ -1,5 +1,25 @@
 package com.Command;
 
-public class AddTrack {
+import com.Model.Library;
+import com.Model.Track;
+
+public class AddTrack implements ICommand {
+    private final Library receiver;
+    private Track track;
+
+    public AddTrack(Library receiver, Track track) {
+        this.receiver = receiver;
+        this.track = track;
+    }
+
+    @Override
+    public void execute() {
+        this.receiver.addTrack(this.track);
+    }
+
+    @Override
+    public void undo() {
+        this.receiver.removeTrack(this.track);
+    }
     
 }
--- src/main/java/com/Command/ModifyTrack.java
+++ src/main/java/com/Command/ModifyTrack.java
@@ -1,5 +1,49 @@
 package com.Command;
 
-public class ModifyTrack {
-    
+import com.Model.Track;
+import com.Model.Library;
+
+public class ModifyTrack implements ICommand {
+    private final Library receiver;
+    private final Track trackToModify;
+
+    private final String oldTitle, oldAuthor, oldGenre, oldAlbum, oldFilePath;
+    private final int oldYear, oldDuration;
+
+    private final String newTitle, newAuthor, newGenre, newAlbum, newFilePath;
+    private final int newYear, newDuration;
+
+    public ModifyTrack(Library receiver, Track trackToModify, String newTitle, String newAuthor, int newYear,
+            String newGenre, int newDuration, String newAlbum, String newFilePath) {
+        this.receiver = receiver;
+        this.trackToModify = trackToModify;
+
+        // Salvo i vecchi valori per poter eseguire l'undo
+        this.oldTitle = trackToModify.getTitle();
+        this.oldAuthor = trackToModify.getAuthor();
+        this.oldYear = trackToModify.getYear();
+        this.oldGenre = trackToModify.getGenre();
+        this.oldDuration = trackToModify.getDuration();
+        this.oldAlbum = trackToModify.getAlbum();
+        this.oldFilePath = trackToModify.getFilePath();
+
+        // Nuovi valori da impostare
+        this.newTitle = newTitle;
+        this.newAuthor = newAuthor;
+        this.newYear = newYear;
+        this.newGenre = newGenre;
+        this.newDuration = newDuration;
+        this.newAlbum = newAlbum;
+        this.newFilePath = newFilePath;
+    }
+
+    @Override
+    public void undo() {
+        receiver.updateTrack(trackToModify, newTitle, newAuthor, newYear, newGenre, newDuration, newAlbum, newFilePath);
+    }
+
+    @Override
+    public void execute() {
+        receiver.updateTrack(trackToModify, oldTitle, oldAuthor, oldYear, oldGenre, oldDuration, oldAlbum, oldFilePath);
+    }
 }
