b8310ebba87a72754aa561b9fa126f51c12f5fec
Michela <m.gaeta29@studenti.unisa.it>
2026-06-02 17:09:58 +0200

    Aggiunta commenti + correzioni test

--- src/test/java/com/Model/LibraryTest.java
+++ src/test/java/com/Model/LibraryTest.java
@@ -6,6 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * @brief Test per l'entità Library. Verifica le funzionalità principali della
+ *        classe Library, come l'aggiunta, la rimozione e l'aggiornamento dei
+ *        brani (Track) al suo interno.
+ */
 public class LibraryTest {
     private Library l;
     private Track t;
@@ -13,6 +18,10 @@ public class LibraryTest {
     private Track track2;
     private Track track3;
 
+    /**
+     * @brief inizializza l'ambiente di test, crendo una istanza vuota di library e
+     *        popolando le track con dati fittizi.
+     */
     @BeforeEach
     public void setUp() {
         l = new Library();
@@ -23,6 +32,12 @@ public class LibraryTest {
         track3 = new Track("Track 3", "Artist 3", 2022, "Genre 3", 180, "Album 3", "path/to/track3.mp3");
     }
 
+    /**
+     * @brief testa l'aggiunta di un brano alla libreria, dopo aver aggiunto il
+     *        brano, il conteggio totale deve aumentare di uno e il brano specifico
+     *        deve essere contenuto nella collezione.
+     */
+
     @Test
     public void testAddTrack_success() {
         l.addTrack(t);
@@ -30,6 +45,11 @@ public class LibraryTest {
         assertTrue(l.getLibrary().contains(t), "La libreria dovrebbe contenre il brano");
     }
 
+    /**
+     * @brief Testa la rimozione di un brano dalla libreria. Inserisce 3 brani, ne
+     *        rimuove uno, verifica che il conteggio di aggiorni correttamente e che
+     *        il brano non sia contenuto più nella collezione.
+     */
     @Test
     public void testRemoveTrack_success() {
         l.addTrack(track1); // dim=1
@@ -37,19 +57,25 @@ public class LibraryTest {
         l.addTrack(track3); // dim=3
         l.removeTrack(track1);
         assertEquals(2, l.getTracksCount());
+        assertTrue(!l.getLibrary().contains(track1), "La libreria non dovrebbe contenere più il brano");
     }
 
+    /**
+     * @brief Testa l'aggiornamento dei dati relativi a un brano esistente. Aggiunge
+     *        un brano, ne modifica i campi e verifica che i gettere restituiscano i
+     *        valori aggiornati.
+     */
     @Test
     public void testUpdateTrack_success() {
         l.addTrack(track1);
         l.updateTrack(track1, "esempio1", "esempio2", 2020, "esempio3", 0, "esempio4", "esempio5.mp3");
-        assertEquals("esempio1", t.getTitle(), "Il titolo dovrebbe essere cambiato");
-        assertEquals("esempio2", t.getAuthor(), "L'autore dovrebbe essere cambiato");
-        assertEquals(2020, t.getYear(), "L'anno dovrebbe essere cambiato");
-        assertEquals("esempio3", t.getGenre(), "Il genere dovrebbe essere cambiato");
-        assertEquals(0, t.getDuration(), "La durata dovrebbe essere cambiata");
-        assertEquals("esempio4", t.getAlbum(), "L'album dovrebbe essere cambiato");
-        assertEquals("esempio5.mp3", t.getFilePath(), "Il path dovrebbe essere cambiato");
+        assertEquals("esempio1", track1.getTitle(), "Il titolo dovrebbe essere cambiato");
+        assertEquals("esempio2", track1.getAuthor(), "L'autore dovrebbe essere cambiato");
+        assertEquals(2020, track1.getYear(), "L'anno dovrebbe essere cambiato");
+        assertEquals("esempio3", track1.getGenre(), "Il genere dovrebbe essere cambiato");
+        assertEquals(0, track1.getDuration(), "La durata dovrebbe essere cambiata");
+        assertEquals("esempio4", track1.getAlbum(), "L'album dovrebbe essere cambiato");
+        assertEquals("esempio5.mp3", track1.getFilePath(), "Il path dovrebbe essere cambiato");
 
     }
 
